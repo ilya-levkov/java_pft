52ddf4f79141dac79e12ad3747c7d2e4dd98468c
ilya-levkov <ilya.levkov2@gmail.com>
2017-07-21 01:34:36 +0300

    Функции заменены на методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,24 +8,15 @@ public class MyFirstProgram {
         hello("Ilya");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
 
-        System.out.println("Плозадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        System.out.println("Плозадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
-
 
 }
