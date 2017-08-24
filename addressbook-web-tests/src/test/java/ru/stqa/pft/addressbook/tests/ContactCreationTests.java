837b708be189751b8d261aad4f194a0ead81745a
ilya-levkov <ilya.levkov2@gmail.com>
2017-08-24 14:20:06 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,9 +7,11 @@ public class ContactCreationTests extends TestBase{
 
     @Test
     public void testContactCreation() {
+        app.getNavigationHelper().gotoToHomePage();
         app.getContactHelper().gotoContactCreationPage();
         app.getContactHelper().fillContactForm(new ContactData("Ilya", "Levkov", "Russia, Ryazan", "+79156255826", "ilya.levkov2@gmail.com", "test1"), true);
         app.getContactHelper().submitContactCreation();
+        app.getContactHelper().returnToHomePage();
     }
 
 }
