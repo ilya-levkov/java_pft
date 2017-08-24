1bc69fcd2fd60eca5da40a9f23a370a178d7c388
ilya-levkov <ilya.levkov2@gmail.com>
2017-08-25 02:03:20 +0300

    Контроль количества элементов в коллекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        /*
+        String[] langs = new String[4];
+        langs[0] = "Java";
+        langs[1] = "C#";
+        langs[2] = "Python";
+        langs[3] = "PHP";
+        */
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+        List<String> languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("C#");
+        languages.add("Python");
+
+        for (String l : langs) {
+            System.out.println("Я хочу выучить " + l);
+        }
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
+        }
+    }
+}
